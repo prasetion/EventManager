1fb768434bd9bb1cb8ca46fd97f7652698f53ef0
prasetion <prasetion@gmail.com>
2024-08-27 22:57:56 +0700

    using delegate or function for listen event

--- Assets/Scripts/EventListener.cs
+++ Assets/Scripts/EventListener.cs
@@ -5,22 +5,35 @@ using UnityEngine;
 
 public class EventListener : MonoBehaviour
 {
+    private delegate void EventDelegate();
+
     // Start is called before the first frame update
     void Start()
     {
         EventManager<string>.RegisterEvent("testEvent", OnClick);
+        EventManager<EventDelegate>.RegisterEvent("eventDelegate", OnEventDelegate);
+    }
+
+    private void OnEventDelegate(EventDelegate eventDelegate)
+    {
+        // throw new NotImplementedException();
+        Debug.Log("Event Delegate");
     }
 
     private void OnClick(string response)
     {
         // throw new NotImplementedException();
         Debug.Log("Event triggered with response: " + response);
+        EventManager<string>.UnregisterEvent("testEvent", OnClick);
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyUp(KeyCode.Space))
+        {
             EventManager<string>.TriggerEvent("testEvent", "ini adalah response");
+            EventManager<EventDelegate>.TriggerEvent("eventDelegate", null);
+        }
     }
 }
